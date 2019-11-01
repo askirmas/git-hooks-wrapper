4c6251048ab505598dac74cb087a0936f7b5220f
askirmas <kirmas@phystech.edu>
2019-11-01 21:39:51 +0200

    Update init with folder making

--- hooks/init.sh
+++ hooks/init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-GIT_PATH=$(git config --local --get core.hookPath || echo ".git/hooks")
 MY_DIR=$(dirname ${BASH_SOURCE[0]})
 
+GIT_PATH=$(git config --local --get core.hookPath || echo ".git/hooks")
+
 ln -s $GIT_PATH/pre-commit $MY_DIR/pre-commit
+
+HOOKS_DIR=$(cat $MY_DIR/hooks_dir)
+
+mkdir $HOOKS_DIR >/dev/null 2>&1
