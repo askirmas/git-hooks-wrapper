e0a7af99545ed0eaf2b862fb4de5522f8fabb453
askirmas <kirmas@phystech.edu>
2019-11-21 00:42:04 +0200

    Fix init typo and avoid get hooks

--- hooks/init.sh
+++ hooks/init.sh
@@ -3,14 +3,14 @@ MY_DIR=$(dirname "$(realpath "$0")")
 
 if [ -z "$1" ]
 then
-  HOOKS_PATH=$(git config --local --get core.hookPath || echo "./git-hooks")
+  HOOKS_PATH="./git-hooks"
 else
   HOOKS_PATH=$1
 fi
 
 echo $HOOKS_PATH > "$MY_DIR/hooks_dir"
 
-git config --local core.hookPath $MY_DIR
+echo $(git config --local core.hooksPath $MY_DIR)
 
 echo "Wrapped git hooks in folder '$HOOKS_PATH'"
 exit 0;
