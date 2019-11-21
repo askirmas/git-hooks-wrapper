180fad83551c6926a506fd42139b404d64ba5be3
askirmas <kirmas@phystech.edu>
2019-11-21 03:07:46 +0200

    Update init message

--- hooks/init.sh
+++ hooks/init.sh
@@ -3,14 +3,14 @@ MY_DIR=$(dirname "$(realpath "$0")")
 
 if [ -z "$1" ]
 then
-  HOOKS_PATH="./git-hooks"
+  HOOKS_PATH="git-hooks"
 else
   HOOKS_PATH=$1
 fi
 
 echo $HOOKS_PATH > "$MY_DIR/hooks_dir"
 
-echo $(git config --local core.hooksPath $MY_DIR)
+git config --local core.hooksPath $MY_DIR
 
-echo "Wrapped git hooks in folder '$HOOKS_PATH'"
+echo "Wrapped git hooks in folder '$HOOKS_PATH': $(git config --local --get core.hooksPath)"
 exit 0;
