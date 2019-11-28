0f4f2eae43620233d95896ac6e32a57a23768c75
askirmas <kirmas@phystech.edu>
2019-11-28 03:11:04 +0200

    Recover 1 lined hook dir storing

--- hooks/init.sh
+++ hooks/init.sh
@@ -8,9 +8,9 @@ else
   HOOKS_PATH=$1
 fi
 
-echo "$HOOKS_PATH" > "$MY_DIR/hooks_dir"
+echo -n "$HOOKS_PATH" > "$MY_DIR/hooks_dir"
 
-git config --local core.hooksPath $MY_DIR
+git config --local core.hooksPath "$MY_DIR"
 
 echo "Wrapped git hooks in folder '$HOOKS_PATH': $(git config --local --get core.hooksPath)"
 exit 0;
