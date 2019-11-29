cab4caba148a243eb00e98e882575c598f9042d1
askirmas <kirmas@phystech.edu>
2019-11-29 15:06:59 +0200

    Update init erroring

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MY_DIR=$(dirname "$(realpath "$0")")
 CMD="$1"; shift
-test -e "$MY_DIR/hooks/$CMD" && ($MY_DIR/hooks/$CMD $@; exit $?)
-test -e "$MY_DIR/scripts/$CMD.sh" && ($MY_DIR/scripts/$CMD.sh $@; exit $?)
+test -e "$MY_DIR/hooks/$CMD" && ($MY_DIR/hooks/$CMD $@ >&1; exit $?)
+test -e "$MY_DIR/scripts/$CMD.sh" && ($MY_DIR/scripts/$CMD.sh $@ >&1; exit $?)
 exit $?
